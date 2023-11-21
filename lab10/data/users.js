d45ca42cf3bfc72b250bf87f6411e9f74d3b977d
Roger Shagawat <34222430+rogerShagawat@users.noreply.github.com>
2023-11-21 08:02:13 -0500

    lab10 stub

---
+++ lab10/data/users.js
@@ -0,0 +1,10 @@
+//import mongo collections, bcrypt and implement the following data functions
+export const registerUser = async (
+  firstName,
+  lastName,
+  emailAddress,
+  password,
+  role
+) => {};
+
+export const loginUser = async (emailAddress, password) => {};
